3930fc3f3054475516717dbf4c4fd5101cb6a1f2
Alaakamal1 <a.qeshta3574@su.edu.eg>
2025-06-17 22:24:04 +0300

    Local changes before pulling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,35 +3,29 @@ import { RouterModule, Routes } from '@angular/router';
 import { CartpageComponent } from './pages/cartpage/cartpage.component';
 import { ProductListComponent } from './components/specific/product-list/product-list.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
-
 import { RegisterFormComponent } from './pages/register-form/register-form.component';
 import { LoginFormComponent } from './pages/login-form/login-form.component';
-
 import { AboutusComponent } from './pages/aboutus/aboutus.component';
 import { ContactusComponent } from './pages/contactus/contactus.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { HomeComponent } from './pages/home/home.component';
 
-
 const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'cart',component:CartpageComponent},
   {path:'products',component:ProductListComponent},
-
  { path: 'products/:id', component: ProductDetailComponent },
- {path: 'register', component: RegisterFormComponent },
+ {path: '', component: RegisterFormComponent },
  {path: 'login', component: LoginFormComponent },
-
   {path: 'products/:id', component: ProductDetailComponent },
   {path:'aboutus',component:AboutusComponent},
   {path:'contactus',component:ContactusComponent},
   {path:'**',component:NotFoundComponent}
 
-
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
