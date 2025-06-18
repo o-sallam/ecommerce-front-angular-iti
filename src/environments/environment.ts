e9f3eb905484695dcb7cbd4a3f693dc96e310646
o-sallam <omar.sallam.dev@gmail.com>
2025-06-18 15:36:25 +0200

    edit(linke of home from /home to / , env file remove the lst /

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:3000',
-  apiUrl: 'https://ecommerce-api-core-iti.vercel.app/',
+  apiUrl: 'https://ecommerce-api-core-iti.vercel.app',
 };
